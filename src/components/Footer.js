56e397c189bd65c86b0b83e3b42609b666e705bd
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-03 11:04:27 -0600

    rafactor app. js into three components

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = (props) => {
+  return (
+    <p className="App-footer">copyright 2018.</p>
+  );
+};
+
+export default Footer;
---
+++ src/components/Index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Index = (props) => {
+  return (
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload the page.
+    </p>
+  );
+};
+
+export default Index;
